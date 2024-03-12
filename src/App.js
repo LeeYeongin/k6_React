951afec609de914a2937bf2decbcd9bbf10ca8f6
LeeYeongin <youngin5757@gmail.com>
2024-03-12 10:41:53 +0900

    component만들기

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
-// component명은 대문자로 작성(일반 html태그와 구분하기 위해서)
+import MainHeader from './01/MainHeader';
+// component명은 대문자로 작성(일반 html태그와 구분하기 위해서), 함수형으로 작성
+// component는 반드시 return을 가져야함
 function App() {
-  return ( // 부모태그 하나밖에 return 못함
+  return ( // 부모태그 하나밖에 return 못함 -> 추가하고 싶을때, fragment(<></>)로 묶어 사용
     <>
       <div className="App"> {/* class는 claasName으로 작성*/}
-        <header className="App-header">
-          <p>
-            k-digital 6기 이영인
-          </p>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        <MainHeader/>
       </div>
-      <div>402호</div> {/* 부모태그는 하나만 가능 -> fragment(<></>)로 묶어 사용*/}
+      <div>402호</div>
     </>
   );
 }
 
-export default App;
+// 화살표 함수로 작성가능
+// const App = () => {
+//   return ();
+// }
+
+// 모듈형 함수이기 때문에
+// 이 모듈을 외부에서 사용하기 위해 export 해줘야함
+// 중괄호 쓰지 않고 사용가능
+export default App; 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css';  // stylesheet도 import하여 사용(link X)
 import App from './App'; // App.js는 component
 // import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render( // html태그가 return됨
+const root = ReactDOM.createRoot(document.getElementById('root')); // 가상돔 생성
+root.render( // html태그(사용자 정의 태그)가 return됨
   <React.StrictMode>
-    {/* 사용자 정의태그 - 종료태그 필수*/}
-    <App />  {/* component명은 대문자로 작성(일반 html태그와 구분하기 위해서) */}
+    <App/>
   </React.StrictMode>
 );
 
+/* 
+  <App/>
+  사용자 정의태그 - 종료태그 필수
+  component명은 대문자로 시작(일반 html태그와 구분하기 위해서)
+*/
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
