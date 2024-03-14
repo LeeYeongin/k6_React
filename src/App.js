2d1a018616ff512d7fa4622d1e56c647a76168ea
LeeYeongin <youngin5757@gmail.com>
2024-03-14 12:39:22 +0900

    icon 사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 // import './App.css';
 import HelloCss from './02/HelloCss';
+import { FaHome } from "react-icons/fa";
 
 function App() {
   return ( 
-    <div>
-      
+    <div className='flex flex-col 
+                    w-full max-w-screen-xl
+                    h-screen 
+                    mx-auto
+                    overscroll-y-auto'>
+      <header className='flex justify-between items-center
+                         h-20
+                         p-10
+                         text-xl font-bold text-slate-200
+                         bg-slate-400'>
+        <div className='flex justify-center items-center'>
+          리액트실습
+        </div>
+        <div><FaHome className='text-3xl'/></div>
+      </header>
+      <main className='grow'>
+        <HelloCss/>
+      </main>
+      <footer className='flex justify-center items-center
+                        h-20 bg-slate-700
+                        text-white text-base'>
+        @ 2024 LeeYeongIn. All right reserved.
+      </footer>
     </div>
   );
 }
