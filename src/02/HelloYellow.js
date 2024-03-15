3196693404b130f9d70db4d2711b066d5569bf89
LeeYeongin <youngin5757@gmail.com>
2024-03-15 14:21:18 +0900

    문구수정

--- src/02/HelloYellow.js
+++ src/02/HelloYellow.js
@@ -9,8 +9,7 @@ export default function HelloYellow() {
     return (
         // 개발자 툴에서 확인해보면 class는 module이 알아서 정해줌을 볼 수 있음
         <div className="flex justify-center align-middle">
-            <FaReact className="text-sky-300"/>
-            <h1 className={styles.ch1}>안녕하세요.</h1> 
+            <h1 className={styles.ch1}>리액트 실습 게시판입니다.</h1> 
         </div>
 
         // inline 스타일을 적용하면 가장 우선순위로 적용
