41c3cc98e9f9d57c0a2cf7e8b39d185d6f318bf7
LeeYeongin <youngin5757@gmail.com>
2024-03-18 15:13:31 +0900

    useState Hook

--- src/04/MyList.js
+++ src/04/MyList.js
@@ -1,13 +1,20 @@
 import { FaHeart } from "react-icons/fa6";
+import { useState } from "react"; // 상태(state)변수
+
 export default function MyList({ title, imgUrl, content }) {
+    // 지역변수
     let cnt=0;
+
+    // state 변수 : const[변수명, 값을 바꿀 setter] = useState(초기값);
+    const [stCnt, setStCnt] = useState(0);
 
     const handleLike = (t) => {
         console.log('handleLike' + t);
         cnt = cnt+1;
         console.log(`cnt = ${cnt}`)
+        setStCnt(stCnt+1);
     }
-    
+
     return (
         <div className="w-full flex
                         border border-l-slate-300 rounded
@@ -31,9 +38,10 @@ export default function MyList({ title, imgUrl, content }) {
                         <FaHeart className="text-orange-400 hover:text-red-600 hover:cursor-pointer"/>
                     </span>
                     <span className="mx-2">좋아요</span>
-                    <span>{cnt}</span>
+                    <span>{stCnt}</span>
                 </p>
             </div>
         </div>
     )
 }
+// useState hook
